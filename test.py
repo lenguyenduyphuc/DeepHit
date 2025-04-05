c838a2fb22e2356248bf3f120d05429da6539d1e
Phuc <duyphuclenguyen@gmail.com>
2025-04-04 22:00:47 -0400

    Cleaned

--- test.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-print("Available devices:")
-for device in tf.config.list_physical_devices():
-    print(device)
-# Or in TF 1.x:
-from tensorflow.python.client import device_lib
-print(device_lib.list_local_devices())
